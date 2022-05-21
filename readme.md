50a6ff9a375544f5f182331a371bde714ff352a1
Moustafa Mohsen <moustafamohsen1@gmail.com>
2022-05-21 23:54:44 +0200

    basic struct

---
+++ readme.md
@@ -0,0 +1,41 @@
+# SellinBot Nodejs 🔐
+
+The backend server for SellinBot, sell and checkout customers on the major chat apps, sellinbot.com
+
+
+## Testing
+
+### Build command 
+
+    npm run build
+
+
+### serve commands
+
+    npm run watch
+    npm run serve
+
+add a file called `.env`  with the following the environment variables
+
+```
+APP_SECRET_KEY=VARIABLE
+DATABASE_URL=VARIABLE
+DATABASE_PASSWORD=VARIABLE
+DATABASE_USER=VARIABLE
+DATABASE_HOST=VARIABLE
+DATABASE_PORT=VARIABLE
+DATABASE_NAME=VARIABLE
+NODEJS_PORT=VARIABLE
+```
+Replace the word VARIABLE with the appropriate value
+
+
+## Links:
+
+Website: https://sellinbot.com
+
+App: https://sellinbot.com/app/
+
+Backend repo: https://github.com/MoustafaMohsen/sellinbot-server
+
+FrontEnd repo: https://github.com/RoqayaMourad/sellinbot-ionic
---
+++ src/server/server-routes.ts
@@ -0,0 +1,105 @@
+import { HelperService } from './../services/util/helper';
+import { IUser } from './../interfaces/user.interface';
+import performance from "perf_hooks";
+import express from "express";
+import MainServerCore from './core/server-core';
+import { DBService } from "../services/dbservice";
+import { UserService } from '../services/user.service';
+
+export default class MainServerRoutes extends MainServerCore {
+
+    setupRoute() {
+
+        function send(res: express.Response, data, t0) {
+            let pre = performance.performance.now() - t0;
+            console.log(`-->Request for:'${res.req.path}', from client:'${res.req.ip}' took:${pre}ms`);
+            if (!res.headersSent) {
+                res.send(JSON.stringify({ performance: pre, success: true, data }))
+            } else {
+                res.write(JSON.stringify({ performance: pre, success: true, data }));
+                res.end();
+            }
+        }
+
+        function err(res: express.Response, message, t0, statuscode = 400) {
+            // res.status(statuscode);
+            let pre = performance.performance.now() - t0;
+            console.log(`-->Request errored for:'${res.req.path}', from client:'${res.req.ip}' took:${pre}ms`);
+            console.error(message);
+            res.send(JSON.stringify({ data: {}, response_status: 400, message, performance: pre, success: false }))
+        }
+
+        //#region Admin Area
+        this.app.get('/', async (req, res) => {
+            let t0 = performance.performance.now();
+            let data = {} as any;
+            try {
+                send(res, data, t0)
+            } catch (error) {
+                err(res, error, t0)
+            }
+        })
+        this.app.get('/admin/test-db', async (req, res) => {
+            let t0 = performance.performance.now();
+            let data = {} as any;
+            const db = new DBService();
+            try {
+                data.result = (await db.connect());
+                send(res, data, t0)
+            } catch (error) {
+                err(res, error, t0)
+            }
+        })
+
+        this.app.post('/admin/prepare-db/' + process.env.APP_SECRET_KEY, async (req, res) => {
+            let t0 = performance.performance.now();
+            let data = {} as any;
+            const db = new DBService();
+            try {
+                data.result = { ...(await db.PrepareDB(req.body.database)) };
+                send(res, data, t0)
+            } catch (error) {
+                err(res, error, t0)
+            }
+        })
+        //#endregion
+
+
+        this.app.post('/register', async (req, res) => {
+            let t0 = performance.performance.now();
+            try {
+                const userSrv = new UserService();
+                let newUserObject: IUser = req.body.secureAuthObject;
+                newUserObject.meta = {
+                    private_key: HelperService.makeid(64)
+                }
+                userSrv.registerUser(newUserObject).then((d) => {
+                    delete d.meta;
+                    send(res, d, t0)
+                }).catch(e => {
+                    err(res, e, t0)
+                })
+            } catch (error) {
+                err(res, error, t0)
+            }
+        })
+
+        this.app.post('/login', async (req, res) => {
+            let t0 = performance.performance.now();
+            try {
+                const userSrv = new UserService();
+                let secureAuthObject: IUser = req.body.secureAuthObject;
+                userSrv.authenticatUser(secureAuthObject).then((d) => {
+                    delete d.meta;
+                    send(res, d, t0)
+                }).catch(e => {
+                    err(res, e, t0)
+                })
+            } catch (error) {
+                err(res, error, t0)
+            }
+        })
+
+    }
+
+}
