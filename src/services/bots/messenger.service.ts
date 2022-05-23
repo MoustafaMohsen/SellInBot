78f10590e56a78ec85a4b3d3c2541e4def392869
Moustafa Mohsen <moustafamohsen1@gmail.com>
2022-05-23 23:00:07 +0200

    basic messenger bot configs

--- src/services/bots/messenger.service.ts
+++ src/services/bots/messenger.service.ts
@@ -1,3 +1,26 @@
-export class MessengerBot{
-    
+/// <reference path="./../../../node_modules/facebook-bot-messenger/index.js"/>
+import * as MessengerPlatform from "facebook-bot-messenger"
+
+export class MessengerBot {
+    bot
+    constructor() {
+        MessengerPlatform
+    }
+    setupBot() {
+        this.bot = MessengerPlatform.create({
+            pageID: '<your page id>',
+            appID: '<your app id>',
+            appSecret: '<your app secret>',
+            validationToken: '<your validation token>',
+            pageToken: '<your page token>'
+        });
+        this.bot.webhook('/webhook');
+
+        this.bot.on(MessengerPlatform.Events.MESSAGE, function (userId, message) {
+            // add code below.
+        });
+
+        this.bot.listen(8080);
+
+    }
 }
