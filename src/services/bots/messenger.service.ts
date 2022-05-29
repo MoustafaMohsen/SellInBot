6a8e7e34768b7788838d0a4ce172409fd2e59056
Moustafa Mohsen <moustafamohsen1@gmail.com>
2022-05-29 20:41:51 +0200

    bot massenger conversations examples

--- src/services/bots/messenger.service.ts
+++ src/services/bots/messenger.service.ts
@@ -20,44 +20,40 @@ export class MessengerBot {
         this.bot = new BootBot(configs_for_page);
         const bot = this.bot
         bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
-            // Send a text message followed by another text message that contains a typing indicator
-            chat.say('Hello, human friend!').then(() => {
-                chat.say('How are you today?', { typing: true });
+            console.log(payload, chat);
+            chat.say('Hello, human friend!', { typing: true }).then(() => {
+                chat.say({
+                    text: 'What would you like to do today?',
+                    quickReplies: ['Purchase a Product', 'Check order status', 'Browse Products']
+                });
             });
+
         });
 
-        bot.hear(['food', 'hungry'], (payload, chat) => {
+        bot.hear(['Purchase a Product'], (payload, chat) => {
             // Send a text message with quick replies
             chat.say({
-                text: 'What do you want to eat today?',
-                quickReplies: ['Mexican', 'Italian', 'American', 'Argentine']
+                text: 'Please enter product Name or Code?'
             });
         });
 
         bot.hear(['help'], (payload, chat) => {
             // Send a text message with buttons
-            chat.say({
-                text: 'What do you need help with?',
-                buttons: [
-                    { type: 'postback', title: 'Settings', payload: 'HELP_SETTINGS' },
-                    { type: 'postback', title: 'FAQ', payload: 'HELP_FAQ' },
-                    { type: 'postback', title: 'Talk to a human', payload: 'HELP_HUMAN' }
-                ]
-            });
+
         });
 
         bot.hear('image', (payload, chat) => {
-            // Send an attachment
-            chat.say({
-                attachment: 'image',
-                url: 'https://www.adobe.com/express/feature/image/media_16ad2258cac6171d66942b13b8cd4839f0b6be6f3.png?width=750&format=png&optimize=medium'
-            });
         });
 
-        bot.start();
+        bot.start(process.env.MESSENGER_PORT);
         bot.on('message', (payload, chat) => {
             console.log(payload);
-            
+
+            // chat.say(`Echo: ${text}`);
+        });
+        bot.on('message', (payload, chat) => {
+            console.log(payload);
+
             // chat.say(`Echo: ${text}`);
         });
 
@@ -65,3 +61,44 @@ export class MessengerBot {
     }
 
 }
+
+
+/*
+chat.say({
+
+    text: 'What do you need help with?',
+    buttons: [
+        { type: 'postback', title: 'Settings', payload: 'HELP_SETTINGS' },
+        { type: 'postback', title: 'FAQ', payload: 'HELP_FAQ' },
+        { type: 'postback', title: 'Talk to a human', payload: 'HELP_HUMAN' }
+    ]
+})
+
+chat.say({
+    attachment: 'image',
+    url: 'https://www.adobe.com/express/feature/image/media_16ad2258cac6171d66942b13b8cd4839f0b6be6f3.png?width=750&format=png&optimize=medium'
+});
+
+chat.say({
+    text: 'What would you like to do today?',
+    quickReplies: ['Purchase a Product', 'Check order status', 'Browse Products']
+});
+
+// Start a conversation
+const askName = (convo) => {
+		convo.ask(`What's your name?`, (payload, convo) => {
+			const text = payload.message.text;
+			convo.set('name', text);
+			convo.say(`Oh, your name is ${text}`).then(() => sendSummary(convo));
+		});
+	};
+const sendSummary = (convo) => {
+		convo.say(`Ok, here's what you told me about you:
+	      - Name: ${convo.get('name')}
+      convo.end();
+	};
+
+chat.conversation((convo) => {
+    askName(convo);
+});
+*/
